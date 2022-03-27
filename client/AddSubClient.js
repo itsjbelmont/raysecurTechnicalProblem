8a791b0ab7d5d071580d2c9628a648dc62738723
jackbelmont <jackbelmont8@gmail.com>
2022-03-27 17:08:04 -0400

    Update client to send more requests

--- client/AddSubClient.js
+++ client/AddSubClient.js
@@ -7,26 +7,26 @@ function AddSubClient(url, objToSend) {
     // Member Variables
     this.url = url;
     this.objToSend = objToSend
-    this.ws = new WebSocket(this.url);
+    try {
+        this.ws = new WebSocket(this.url);
+    } catch (e) {
+        console.log("Exception thrown when trying to create a new WebSocket object with url: " + this.url)
+    }
 
     // Setup the event handlers
     this.ws.onopen = () => this.onAddSubClientConnect();
     this.ws.onmessage = (event) => this.onAddSubClientMessage(event);
-    this.ws.onclose = () => this.onAddSubClientClose();
 }
 
 AddSubClient.prototype.onAddSubClientConnect = function () {
-    console.log("Connected to websocket: " + this.url)
+    // When we connect to the websocket send our object
     this.sendObjAsJson(this.objToSend);
 }
 
 AddSubClient.prototype.onAddSubClientMessage = function (event) {
-    console.log("Recieved message from websocket: " + event.data);
+    // Log the message and exit the connection
+    console.log("Recieved message from websocket: " + event.data + "\n");
     this.ws.close();
-}
-
-AddSubClient.prototype.onAddSubClientClose = function () {
-    console.log("Connection closed to websocket: " + this.url);
 }
 
 AddSubClient.prototype.sendObjAsJson = function (obj) {
--- client/ClientMain.js
+++ client/ClientMain.js
@@ -5,12 +5,36 @@ const WebSocket = require('ws');
 var AddSubRequest = require('./AddSubRequest');
 var AddSubClient = require('./AddSubClient');
 const { lstat } = require('fs');
+const { syncBuiltinESMExports } = require('module');
 
-// Setup websocket client
-const url = 'ws://localhost:9002';
+// Function to add a bit of delay between requests
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
-addRequest = new AddSubRequest('add', 10, 5);
-addClient = new AddSubClient(url, addRequest);
+// Function to send a bunch of requests to the websocket
+async function connectAndSendRequests() {
+    // Setup websocket client
+    const url = 'ws://localhost:9002';
 
-subRequest = new AddSubRequest('sub', 10, 5);
-subClient = new AddSubClient(url, subRequest);
+    requests = [
+        new AddSubRequest('add', 10, 5), 
+        new AddSubRequest('add', -5, -10),
+        new AddSubRequest('add', 100, 11),
+        new AddSubRequest('add', 1, 0),
+        new AddSubRequest('sub', -1, 5), 
+        new AddSubRequest('sub', -8, -2), 
+        new AddSubRequest('sub', 40, 100), 
+        new AddSubRequest('sub', 100, 87), 
+    ];
+
+    for (i = 0; i < requests.length; i++) {
+        req = requests[i];
+        addClient = new AddSubClient(url, req);
+        await sleep(500);
+    }
+}
+
+connectAndSendRequests();
